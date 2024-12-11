33571f34d35f1970a94968079d224aaad0a0ffa1
BugHunter-X <askzenai@gmail.com>
2024-12-11 07:26:04 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,10 @@
+```javascript
+//Import tailwind directives
+@tailwind base;
+@tailwind components;
+@tailwind utilities;
+
+<div class="bg-red-500 p-4 text-white">
+  This text should be red and white
+</div>
+```
